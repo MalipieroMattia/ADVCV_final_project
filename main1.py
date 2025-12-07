5f8b14552634ada10f729f7ff73169a7f9f5b6b8
fedemarinis <federico.demarinis@outlook.com>
2025-12-07 14:12:03 +0100

    Chanhe path do dataset yaml

--- main1.py
+++ main1.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.pt")
 
 model.train(
-    data="yolo_dataset/dataset.yaml",
+    data="/work/yolo_dataset/dataset.yaml",
     epochs=5,
     imgsz=640,
     batch=8
